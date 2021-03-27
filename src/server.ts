258774892096312443f8807071b43a5105145430
Rakesh DR <rakeshdr543@gmail.com>
2021-03-27 12:20:33 +0530

    Added db connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {createConnection} from "./entities/node_modules/typeorm";
+import {createConnection} from "typeorm";
 import {User} from "./entities/User";
 
 createConnection().then(async connection => {

42b38f595e03a0997e0189234723f85fa4b4aa52
Rakesh DR <rakeshdr543@gmail.com>
2021-03-27 14:24:42 +0530

    Added register user

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import {createConnection} from "typeorm";
 import express from 'express';
 import morgan from "morgan";
 
+import authRoutes from './routes/auth'
+
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.get('/',(req,res)=>{
+app.get('/',(_,res)=>{
     res.send('Hello my reddit')
 })
-
+app.use('/api/auth',authRoutes)
 app.listen(5000,async ()=>{
     console.log("Server connected in 5000")
     try {

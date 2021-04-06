3b82e2f8c90d28f364f5b100b4b7197b646a5efb
Rakesh DR <rakeshdr543@gmail.com>
2021-04-06 11:31:15 +0530

    Added navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,8 @@
-
 import Link from 'next/link'
+import React from "react"
 
 import RedditLogo from '../images/reddit.svg'
-import React from "react";
+
 
 const Navbar:React.FC =()=> (
     <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">

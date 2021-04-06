516bae4c28618516a5c52d12900f5d96896d6566
Rakesh DR <rakeshdr543@gmail.com>
2021-04-06 11:26:25 +0530

    Added navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -17,7 +17,27 @@ const Navbar:React.FC =()=> (
                 <Link href="/">readit</Link>
             </span>
         </div>
-        {/* Serach Input */}
+        {/* Search Input */}
+
+        <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+            <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
+            <input
+                type="text"
+                className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+                placeholder="Search"
+            />
+        </div>
+
+        {/* Auth buttons */}
+        <div className="flex">
+            <Link href="/login">
+                <a className="w-32 py-1 mr-4 leading-5 hollow blue button">log in</a>
+            </Link>
+            <Link href="/register">
+                <a className="w-32 py-1 leading-5 blue button">sign up</a>
+            </Link>
+        </div>
+
     </div>
 )
 

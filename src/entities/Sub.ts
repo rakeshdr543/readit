df3a6d1354fa31337f2c92ed6a59053cafc74ffb
Rakesh DR <rakeshdr543@gmail.com>
2021-04-18 08:29:04 +0530

    Refactored image upload

--- src/entities/Sub.ts
+++ src/entities/Sub.ts
@@ -8,6 +8,7 @@ import {
 import Entity from "./Entity";
 import User from "./User"
 import Post from "./Post";
+import {Expose} from "class-transformer";
 
 @ToEntity('subs')
 export default class Sub extends Entity{
@@ -42,4 +43,17 @@ export default class Sub extends Entity{
     @OneToMany(()=>Post,(post)=>post.sub)
    posts:Post[]
 
+    @Expose()
+    get imageUrl(): string {
+        return this.imageUrn
+            ? `${process.env.APP_URL}/images/${this.imageUrn}`
+            : 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
+    }
+
+    @Expose()
+    get bannerUrl(): string | undefined {
+        return this.bannerUrn
+            ? `${process.env.APP_URL}/images/${this.bannerUrn}`
+            : undefined
+    }
 }

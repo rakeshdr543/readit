c763ea56cd2ec7432451f6461cd088efd5a51240
Rakesh DR <rakeshdr543@gmail.com>
2021-04-24 11:55:49 +0530

    Added create sub

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,16 +5,19 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import useSWR from 'swr'
 import Image from 'next/image'
 
-import { Sub } from '../types'
+import {Post, Sub} from '../types'
 
 import PostCard from '../components/PostCard'
 import Link from 'next/link'
+import {useAuthState} from "../context/auth";
 
 dayjs.extend(relativeTime)
 
 export default function Home() {
-    const { data: posts } = useSWR('/posts')
-    const { data: topSubs } = useSWR('/misc/top-subs')
+    const { data: posts } = useSWR<Post[]>('/posts')
+    const { data: topSubs } = useSWR<Sub[]>('/misc/top-subs')
+
+    const { authenticated } = useAuthState()
 
     return (
         <Fragment>
@@ -43,6 +46,7 @@ export default function Home() {
                                     className="flex items-center px-4 py-2 text-xs border-b"
                                 >
                                         <Link href={`/r/${sub.name}`}>
+                                            <a>
                                             <Image
                                                 src={sub.imageUrl}
                                                 className="rounded-full cursor-pointer"
@@ -50,6 +54,7 @@ export default function Home() {
                                                 width={(6 * 16) / 4}
                                                 height={(6 * 16) / 4}
                                             />
+                                            </a>
                                         </Link>
                                     <Link href={`/r/${sub.name}`}>
                                         <a className="ml-2 font-bold hover:cursor-pointer">
@@ -60,6 +65,15 @@ export default function Home() {
                                 </div>
                             ))}
                         </div>
+                        {authenticated && (
+                            <div className="p-4 border-t-2">
+                                <Link href="/subs/create">
+                                    <a className="w-full px-2 py-1 blue button">
+                                        Create Community
+                                    </a>
+                                </Link>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -163,7 +163,7 @@ const searchSubs = async (req: Request, res: Response) => {
 }
 
 const router= Router()
-router.post('/',auth,createSub)
+router.post('/',user,auth,createSub)
 router.get('/:name', user, getSub)
 router.get('/search/:name', searchSubs)
 router.post(

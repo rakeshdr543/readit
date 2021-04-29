7277b2773ed1ca16be165eafcaa384c631218216
Rakesh DR <rakeshdr543@gmail.com>
2021-04-29 11:35:53 +0530

    Ready for deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,25 +1,21 @@
-{
-   "type": "postgres",
-   "host": "localhost",
-   "port": 5432,
-   "username": "postgres",
-   "password": "root",
-   "database": "readit",
-   "synchronize": true,
-   "logging": true,
-   "entities": [
-      "src/entities/**/*.ts"
-   ],
-   "migrations": [
-      "src/migrations/**/*.ts"
-   ],
-   "subscribers": [
-      "src/subscribers/**/*.ts"
-   ],
-   "seeds": ["src/seeds/**/*{.ts,.js}"],
-   "cli": {
-      "entitiesDir": "src/entities",
-      "migrationsDir": "src/migrations",
-      "subscribersDir": "src/subscribers"
-   }
+const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'build'
+
+module.exports ={
+   type: process.env.DB_DIALECT,
+       host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: false,
+    logging: process.env.NODE_ENV === 'development',
+   entities: [rootDir + '/entities/**/*{.ts,.js}'],
+   migrations: [rootDir + '/migrations/**/*{.ts,.js}'],
+   subscribers: [rootDir + '/subscribers/**/*{.ts,.js}'],
+   seeds: [rootDir + '/seeds/**/*{.ts,.js}'],
+   cli: {
+      entitiesDir: rootDir + '/entities',
+      migrationsDir: rootDir + '/migrations',
+      subscribersDir: rootDir + '/subscribers',
+   },
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -95,7 +95,7 @@ const logout = (_:Request,res:Response)=>{
        'Set-Cookie',
        cookie.serialize('token','',{
           httpOnly:true,
-          secure:process.env.NODE_ENV==='production',
+          // secure:process.env.NODE_ENV==='production',
           sameSite:'strict',
           expires:new Date(0),
           path:'/'
